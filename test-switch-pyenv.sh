89be747ced5c0bd72ac957b3d14e68152a8aabcc
seigot <s.takada.3o3@gmail.com>
2021-07-13 22:23:29 +0900

    test update

--- test-switch-pyenv.sh
+++ test-switch-pyenv.sh
@@ -5,6 +5,22 @@
 # pyenv virtualenv 3.7.7 myenv3.7.7
 # pyenv virtualenv 3.6.9 myenv3.6.9
 
+function test_install() {
+    # sue-robo
+    pip3 install --upgrade pip
+    python3 -m pip install PyQt5
+    pip3 install torch torchvision torchaudio -f https://download.pytorch.org/whl/lts/1.8/torch_lts.html
+    pip3 install --upgrade tensorflow
+    pip3 install tqdm
+    # if you have exec error
+    #   ModuleNotFoundError: No module named '_ctypes'
+    #
+    # fix as follows
+    #   sudo apt-get install libffi-dev 
+    #   pyenv install 3.9.1
+    # https://qiita.com/seigot/items/d4528fc584e4b58b070e
+}
+
 source ~/.bashrc
 
 pyenv activate myenv
@@ -17,6 +33,10 @@ pyenv deactivate
 
 pyenv activate myenv3.7.7
 python -V
+pyenv deactivate
+
+pyenv activate sue-robo_env
+bash start.sh -t 100
 pyenv deactivate
 
 
