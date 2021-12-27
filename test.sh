9823c442f8968ffda580f2d4356151d776ff3b2c
seigot <s.takada.3o3@gmail.com>
2021-12-28 00:38:20 +0900

    update test.sh

--- test.sh
+++ test.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
+function do_game(){
 
-GAME_TIME=10
-RANDOM_SEED=111
-USER_NAME_A="sampleA"
-USER_NAME_B="sampleB"
-COMMAND_A="cd ~/tetris && python3 start.py -s y -l 2 -t ${GAME_TIME} -r ${RANDOM_SEED} -u ${USER_NAME_A}"
-COMMAND_B="cd ~/tetris && python3 start.py -s y -l 2 -t ${GAME_TIME} -r ${RANDOM_SEED} -u ${USER_NAME_B}"
-gnome-terminal -- bash -c "${COMMAND_A}" &
-gnome-terminal -- bash -c "${COMMAND_B}" &
-sleep 1
-WINDOWID_A=`xdotool search --onlyvisible --name "${USER_NAME_A}"`
-WINDOWID_B=`xdotool search --onlyvisible --name "${USER_NAME_B}"`
-xdotool windowmove ${WINDOWID_A} 350 150 &
-xdotool windowmove ${WINDOWID_B} 700 150 &
-sleep ${GAME_TIME}
+    USER_NAME_A="${1}" #"sampleA"
+    USER_NAME_B="${2}" #"sampleB"
+
+    GAME_TIME=10
+    RANDOM_SEED=111
+    COMMAND_A="cd ~/tetris && python3 start.py -s y -l 2 -t ${GAME_TIME} -r ${RANDOM_SEED} -u ${USER_NAME_A}"
+    COMMAND_B="cd ~/tetris && python3 start.py -s y -l 2 -t ${GAME_TIME} -r ${RANDOM_SEED} -u ${USER_NAME_B}"
+
+    # start game
+    gnome-terminal -- bash -c "${COMMAND_A}" &
+    gnome-terminal -- bash -c "${COMMAND_B}" &
+    sleep 1
+
+    # adjust window
+    WINDOWID_A=`xdotool search --onlyvisible --name "${USER_NAME_A}"`
+    WINDOWID_B=`xdotool search --onlyvisible --name "${USER_NAME_B}"`
+    xdotool windowmove ${WINDOWID_A} 350 150 &
+    xdotool windowmove ${WINDOWID_B} 700 150 &
+
+    # sleep until game end
+    sleep ${GAME_TIME}
+}
+do_game "sampleA" "sampleB"
 
 echo "end"
 
