e8d9bfaefcaf09764ff13c9a800f28ecf5b23e15
seigot <s.takada.3o3@gmail.com>
2022-01-04 03:26:54 +0000

    update

--- 20220301/gameserver.sh
+++ 20220301/gameserver.sh
@@ -3,9 +3,25 @@
 # prepare
 #   - install docker
 #   - prepare to push to github
-#       if necessary, see https://qiita.com/azusanakano/items/8dc1d7e384b00239d4d9
+#       if necessary, write ~/.netrc
+#          see https://qiita.com/azusanakano/items/8dc1d7e384b00239d4d9
 #   - export API_KEY="xxx"
 #       if necessary, see https://qiita.com/seigot/items/77cf8ce36023c273232d
+#   - update current idx
+#       $ echo 0 > current_idx.txt
+#   - do main process
+#       $ bash gameserver.sh -m release
+
+## get args level setting
+EXEC_MODE="DEBUG"  # DEBUG or RELEASE
+while getopts m: OPT
+do
+  case $OPT in
+    "m" ) EXEC_MODE="$OPTARG" ;;
+  esac
+done
+echo "mode: ${EXEC_MODE}"
+
 
 function update_result(){
     # update to github
@@ -18,35 +34,45 @@ function update_result(){
     RESULT="$5"
     STR="${DATETIME}, ${REPOSITORY_URL}, ${SCORE}, ${LEVEL}, ${RESULT}"    
 
+    ## update result file
     RESULT_CSV="result.csv"
-    RESULT_LEVEL_CSV="result_level_${LEVEL}.csv"
-    RESULT_RANKING_CSV="result_ranking_level_${LEVEL}.csv"
+    RESULT_LEVEL_LOG="result_level_${LEVEL}.log"
+    RESULT_RANKING_LOG="result_ranking_level_${LEVEL}.log"
 
     echo $STR >> ${RESULT_CSV}
 
     if [ "${RESULT}" == "SUCCESS" ]; then
-	if [ ! -e ${RESULT_LEVEL_CSV} ]; then
-	    echo "DATETIME, REPOSITORY_URL, SCORE, LEVEL, RESULT" >> ${RESULT_LEVEL_CSV}
-	    echo "---" >> ${RESULT_LEVEL_CSV}
+	if [ ! -e ${RESULT_LEVEL_LOG} ]; then
+	    echo "DATETIME, REPOSITORY_URL, SCORE, LEVEL, RESULT" >> ${RESULT_LEVEL_LOG}
+	    echo "---" >> ${RESULT_LEVEL_LOG}
 	fi
-	echo $STR >> ${RESULT_LEVEL_CSV}
-	cat <(head -2 ${RESULT_LEVEL_CSV}) <(tail -n +3 ${RESULT_LEVEL_CSV} | sort -nr -t, -k3 | column -t -s,) > ${RESULT_RANKING_CSV}
+	echo $STR >> ${RESULT_LEVEL_LOG}
+	cat <(head -2 ${RESULT_LEVEL_LOG}) <(tail -n +3 ${RESULT_LEVEL_LOG} | sort -nr -t, -k3 | column -t -s,) > ${RESULT_RANKING_LOG}
     fi
 	
     echo "--"
     cat ${RESULT_CSV}
     echo "--"
-    cat ${RESULT_LEVEL_CSV}
+    cat ${RESULT_LEVEL_LOG}
     echo "--"
-    cat ${RESULT_RANKING_CSV}
-    
+    cat ${RESULT_RANKING_LOG}
+
+    # skip if not release mode
+    if [ "${EXEC_MODE}" != "RELEASE" ];then
+	echo "skip update to git"
+	echo "  EXEC_MODE: ${EXEC_MODE}"
+	return 0
+    fi
+
+    ## update to score server
     #git clone https://github.com/seigot/tetris_score_server
-    #pushd tetris_score_server
-    #git pull
-    #echo $STR >> result.csv
-    #git add result.csv
-    #git commit -m "update"
-    #git push
+    #pushd tetris_score_server/logs
+    git pull
+    git add ${RESULT_CSV}
+    git add ${RESULT_LEVEL_LOG}
+    git add ${RESULT_RANKING_LOG}
+    git commit -m "update result"
+    git push
     #popd
 }
 
@@ -78,7 +104,11 @@ function do_tetris(){
     DATETIME="$1"
     REPOSITORY_URL="$2"
     LEVEL="$3"
-    GAME_TIME="3" # "180"
+    GAME_TIME="180"
+    if [ "${EXEC_MODE}" != "RELEASE" ]; then
+	GAME_TIME="3" # debug value
+    fi 
+    
     PRE_COMMAND="cd ~ && rm -rf tetris && git clone ${REPOSITORY_URL} && cd ~/tetris && pip3 install -r requirements.txt"
     DO_COMMAND="cd ~/tetris && export DISPLAY=:1 && python3 start.py -l ${LEVEL} -t ${GAME_TIME} && jq . result.json"
     POST_COMMAND="cd ~/tetris && jq .judge_info.score result.json"
@@ -126,6 +156,7 @@ function do_polling(){
     JSONFILE="test.json"
     CURRENT_IDX_FILE="current_idx.txt"
 
+    # check if updated
     curl https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/${SHEET}?key=${API_KEY} -o ${JSONFILE}
     RET=$?
     if [ $RET -ne 0 ]; then
@@ -133,7 +164,6 @@ function do_polling(){
 	error_result "-" "-" "0" "-" "curl_google_speadsheet_NG"
 	return 0
     fi
-    #jq . ${JSONFILE}
     VALUE_LENGTH=`jq .values ${JSONFILE} | jq length`
     VALUE_IDX=$((VALUE_LENGTH-1))
     CURRENT_IDX="0"
@@ -144,6 +174,7 @@ function do_polling(){
 	return -1
     fi
 
+    # do tetris
     if [ "${CURRENT_IDX}" -ne "${VALUE_IDX}" ]; then
    
 	for idx in `seq $((CURRENT_IDX+1)) ${VALUE_IDX}`
@@ -151,13 +182,14 @@ function do_polling(){
 	    ## start current idx
 	    echo $idx
 	    echo $idx > ${CURRENT_IDX_FILE}
-	    
-	    ### error check ###
+
+	    # get time
 	    # replace BLANK
 	    VALUE_TIME1=`jq .values[${idx}][0] ${JSONFILE}`
 	    VALUE_TIME2=${VALUE_TIME1//" "/"_"} # blank
 	    VALUE_TIME=${VALUE_TIME2//"\""/""}  # "
 
+	    # get REPOSITORY_URL
 	    # DELETE unnecessary strings
 	    # BLANK_CHECK:
 	    # URL_CHECK: "https://github.com/seigot/tetris"
@@ -178,17 +210,19 @@ function do_polling(){
 		error_result "${VALUE_TIME}" "${VALUE_URL}" "0" "1" "github_url_access_NG"
 		continue
 	    fi
+
+	    # get LEVEL
+	    # replace double quotation
 	    VALUE_LEVEL=`jq .values[${idx}][3] ${JSONFILE} | sed 's/"//g'`
 	    if [ $VALUE_LEVEL == "null" ]; then
 		VALUE_LEVEL=1
 	    fi
 	    
-	    echo $VALUE_TIME
-	    echo $VALUE_URL
-	    echo $VALUE_LEVEL
+	    echo "LEVEL: ${VALUE_TIME}"
+	    echo "URL: ${VALUE_URL}"
+	    echo "LEVEL: ${VALUE_LEVEL}"
 
 	    ## do tetris
-	    #do_tetris "20211229" "https://github.com/seigot/tetris"
 	    do_tetris "${VALUE_TIME}" "${VALUE_URL}" "${VALUE_LEVEL}"
 	done
 
@@ -204,6 +238,12 @@ while true
 do
     do_polling
 
+    if [ "${EXEC_MODE}" != "RELEASE" ];then
+	echo "break polling"
+	echo "  EXEC_MODE: ${EXEC_MODE}"
+	break
+    fi
+    
     SLEEP_SEC=300
     echo "sleep: ${SLEEP_SEC}"
     sleep ${SLEEP_SEC}
