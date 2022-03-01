fa16648082f080cbaf586ea911b79dc4d8bd2200
seigot <s.takada.3o3@gmail.com>
2022-03-01 22:49:34 +0900

    update

--- 20220301/score_attack.sh
+++ 20220301/score_attack.sh
@@ -11,12 +11,13 @@ function do_game(){
     local SCORE_WINDOW_X="${7}"
     local SCORE_WINDOW_Y="${8}"
 
+    local LOGFILE="~/tmp/resultlog_${UNAME}.json"
     local GAME_TIME=10
     local RANDOM_SEED=111
     local PROGRAM_NAME="sample_program"
 
     # prepare
-    local COMMAND="source ~/venvtest/bin/activate && cd ~/tetris && python3 start.py -l ${LEVEL} -t ${GAME_TIME} -r ${RANDOM_SEED} -u ${UNAME}"
+    local COMMAND="source ~/venvtest/bin/activate && cd ~/tetris && python3 start.py -l ${LEVEL} -t ${GAME_TIME} -r ${RANDOM_SEED} -u ${UNAME} -f ${LOGFILE}"
 
     # start sound
 
@@ -24,10 +25,10 @@ function do_game(){
     # start game
     gnome-terminal -- bash -c "${COMMAND}" &
     # download github profile image
-    curl https://avatars.githubusercontent.com/${UNAME} --output "output.png"
-    convert -resize 160x "output.png" "output2.png"
-    python image.py -u ${UNAME} -i "output2.png" &
-    python score.py -u ${UNAME} -p ${PROGRAM_NAME} -l ${LEVEL} &
+    curl https://avatars.githubusercontent.com/${UNAME} --output "${UNAME}.png"
+    convert -resize 160x "${UNAME}.png" "${UNAME}2.png"
+    python image.py -u ${UNAME} -i "${UNAME}2.png" &
+    python score.py -u ${UNAME} -p ${PROGRAM_NAME} -l ${LEVEL} -f ${LOGFILE} &
     sleep 2
 
     # adjust window
@@ -40,16 +41,17 @@ function do_game(){
     local IMAGE_WINDOW_NAME="Image_${UNAME}"
     IMAGE_WINDOWID=`xdotool search --onlyvisible --name "${IMAGE_WINDOW_NAME}"`
     xdotool windowmove ${IMAGE_WINDOWID} ${IMAGE_WINDOW_X} ${IMAGE_WINDOW_Y} &
-    
+
     ## adjust score window
     local SCORE_WINDOW_NAME="Score_${UNAME}"
     SCORE_WINDOWID=`xdotool search --onlyvisible --name "${SCORE_WINDOW_NAME}"`
     xdotool windowmove ${SCORE_WINDOWID} ${SCORE_WINDOW_X} ${SCORE_WINDOW_Y} &
-    
 
     # sleep until game end
     sleep ${GAME_TIME}
     # show result
+
+    return 0
 }
 
 function do_game_main(){
@@ -61,12 +63,11 @@ function do_game_main(){
     do_game ${LEVEL} "isshy-you" 700 150
 }
 
-
-
 echo "start"
 
 #do_game 1 "sampleA" "sampleB"
-do_game 2 "seigot" 500 150 100 50 200 300
+#do_game 2 "seigot" 500 150 100 50 200 300
+do_game 2 "isshy-you" 500 150 100 50 200 300
 
 #do_game 2 "seigot" 350 150 100 100 300 100
 
