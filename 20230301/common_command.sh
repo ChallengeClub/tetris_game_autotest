26d7383e9e2ad552a00f07fc931232c66c80c47a
seigot <s.takada.3o3@gmail.com>
2023-03-14 23:09:11 +0900

    update

--- 20230301/common_command.sh
+++ 20230301/common_command.sh
@@ -22,9 +22,9 @@ function GET_COMMAND(){
 #	curl -LJO https://github.com/seigot/tools/raw/master/tetris/ai_tmp/trained_models/tetris &&\
 #	cp tetris trained_models/."
 #	EXEC_COMMAND="${ADD_COMMAND} && python3 start.py -l ${LEVEL} -d ${DROP_SPEED} -t ${GAME_TIME} -r ${RANDOM_SEED} -u ${UNAME} -f ${LOGFILE} -m predict_sample"
-#	ADD_COMMAND="ls"
-	ADD_COMMAND="curl -LJO https://raw.githubusercontent.com/seigot/tools/master/tetris/20220901_cuda_unavailable.patch &&\
-	patch -p1 < 20220901_cuda_unavailable.patch"
+	ADD_COMMAND="ls"
+#	ADD_COMMAND="curl -LJO https://raw.githubusercontent.com/seigot/tools/master/tetris/20220901_cuda_unavailable.patch &&\
+#	patch -p1 < 20220901_cuda_unavailable.patch"
 	EXEC_COMMAND="${ADD_COMMAND} && python3 start.py -l ${LEVEL} -d ${DROP_SPEED} -t ${GAME_TIME} -r ${RANDOM_SEED} -u ${UNAME} -f ${LOGFILE} -m ${MODE} --predict_weight ${PREDICT_WEIGHT}"
 
     elif [ "${UNAME}" == "neteru141" ]; then
@@ -83,6 +83,24 @@ function GET_COMMAND(){
 	# other
 	EXEC_COMMAND="python3 start.py -l ${LEVEL} -d ${DROP_SPEED} -t ${GAME_TIME} -r ${RANDOM_SEED} -u ${UNAME} -f ${LOGFILE} -m ${MODE} --predict_weight ${PREDICT_WEIGHT}"
     fi
+    echo ${EXEC_COMMAND}
+}
+
+
+function GET_COMMAND_ART(){
+
+    LEVEL=${1}
+    DROP_SPEED=${2}
+    GAME_TIME=${3}
+    RANDOM_SEED=${4}
+    UNAME=${5}
+    LOGFILE=${6}
+    TETRIS_DIR=${7}
+    MODE=${8}
+    ART_CONFIG_FILEPATH=${9}
+
+    # other
+    EXEC_COMMAND="python3 start.py -l ${LEVEL} -d ${DROP_SPEED} -t ${GAME_TIME} -r ${RANDOM_SEED} -u ${UNAME} -f ${LOGFILE} -m ${MODE} --art_config_filepath ${ART_CONFIG_FILEPATH}"
     echo ${EXEC_COMMAND}
 }
 
