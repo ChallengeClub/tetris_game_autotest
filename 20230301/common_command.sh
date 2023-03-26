22d5d53161a1884f77cb96fb579398c6c4e00d4f
seigot <s.takada.3o3@gmail.com>
2023-03-26 23:19:11 +0900

    update

--- 20230301/common_command.sh
+++ 20230301/common_command.sh
@@ -14,74 +14,23 @@ function GET_COMMAND(){
 
     if [ "${UNAME}" == "seigot" ]; then
 	# ex)機械学習用のファイルをダウンロードして、game_managerの下におく
-#	ADD_COMMAND="curl -LJO https://raw.githubusercontent.com/seigot/tools/master/tetris/ai_tmp/game_manager/machine_learning/block_controller_train_sample.py &&\
-#	mv block_controller_train_sample.py game_manager/machine_learning/. &&\
-#	curl -LJO https://raw.githubusercontent.com/seigot/tools/master/tetris/ai_tmp/game_manager/machine_learning/deep_q_network.py &&\
-#	mv deep_q_network.py game_manager/machine_learning/. &&\
-#	mkdir trained_models &&\
-#	curl -LJO https://github.com/seigot/tools/raw/master/tetris/ai_tmp/trained_models/tetris &&\
-#	cp tetris trained_models/."
-#	EXEC_COMMAND="${ADD_COMMAND} && python3 start.py -l ${LEVEL} -d ${DROP_SPEED} -t ${GAME_TIME} -r ${RANDOM_SEED} -u ${UNAME} -f ${LOGFILE} -m predict_sample"
 	ADD_COMMAND="ls"
-#	ADD_COMMAND="curl -LJO https://raw.githubusercontent.com/seigot/tools/master/tetris/20220901_cuda_unavailable.patch &&\
-#	patch -p1 < 20220901_cuda_unavailable.patch"
-	EXEC_COMMAND="${ADD_COMMAND} && python3 start.py -l ${LEVEL} -d ${DROP_SPEED} -t ${GAME_TIME} -r ${RANDOM_SEED} -u ${UNAME} -f ${LOGFILE} -m ${MODE} --predict_weight ${PREDICT_WEIGHT}"
+	EXEC_COMMAND="${ADD_COMMAND} && python3 start.py -l ${LEVEL} -t ${GAME_TIME} -r ${RANDOM_SEED} -u ${UNAME} -f ${LOGFILE} -m ${MODE} --predict_weight ${PREDICT_WEIGHT}"
 
-    elif [ "${UNAME}" == "neteru141" ]; then
-	# neteru141さん用のtetris
-	ADD_COMMAND="curl -LJO https://raw.githubusercontent.com/seigot/tools/master/tetris/ai_tmp/game_manager/machine_learning/block_controller_train_sample.py &&\
-	mv block_controller_train_sample.py game_manager/machine_learning/. &&\
-	curl -LJO https://raw.githubusercontent.com/seigot/tools/master/tetris/ai_tmp/game_manager/machine_learning/deep_q_network.py &&\
-	mv deep_q_network.py game_manager/machine_learning/. &&\
-	mkdir trained_models &&\
-	curl -LJO https://github.com/neteru141/tetris_game/blob/submit/trained_models/tetris?raw=true &&\
-	mv tetris\?raw\=true trained_models/tetris &&\
-	ln -s machine_learning/deep_q_network.py game_manager/deep_q_network.py"
-
-	EXEC_COMMAND="${ADD_COMMAND} && python3 start.py -l ${LEVEL} -d ${DROP_SPEED} -t ${GAME_TIME} -r ${RANDOM_SEED} -u ${UNAME} -f ${LOGFILE} -m predict_sample"
-
-    elif [ "${UNAME}" == "cookie4869" -o "${UNAME}" == "qbi-sui" -o "${UNAME}" == "krymt28" ]; then
-
-	# cuda patch
-	PATCH_NAME="20220901_cuda_unavailable_${UNAME}.patch"
-	ADD_COMMAND="curl -LJO https://raw.githubusercontent.com/seigot/tools/master/tetris/${PATCH_NAME} &&\
-	patch -p1 < ${PATCH_NAME}"
-	EXEC_COMMAND="${ADD_COMMAND} && python3 start.py -l ${LEVEL} -d ${DROP_SPEED} -t ${GAME_TIME} -r ${RANDOM_SEED} -u ${UNAME} -f ${LOGFILE} -m ${MODE} --predict_weight ${PREDICT_WEIGHT}"
-
-
-    elif [ "${UNAME}" == "TsuchiyaYosuke" ]; then
-	# cuda patch
-	PATCH_NAME="20220901_cuda_unavailable_${UNAME}.patch"
-	ADD_COMMAND="curl -LJO https://raw.githubusercontent.com/seigot/tools/master/tetris/${PATCH_NAME} &&\
-	patch -p1 < ${PATCH_NAME} &&\
-	ln -s q_learning game_manager/machine_learning"
-	EXEC_COMMAND="${ADD_COMMAND} && python3 start.py -l ${LEVEL} -d ${DROP_SPEED} -t ${GAME_TIME} -r ${RANDOM_SEED} -u ${UNAME} -f ${LOGFILE} -m ${MODE} --predict_weight ${PREDICT_WEIGHT}"
-	
-
-    elif [ "${UNAME}" == "tuyosi1227" ]; then
-	# cuda patch
-	#PATCH_NAME="20220901_cuda_unavailable_${UNAME}.patch"
-	ADD_COMMAND="ls -l"
-	EXEC_COMMAND="${ADD_COMMAND} && python3 start.py -l ${LEVEL} -d ${DROP_SPEED} -t ${GAME_TIME} -r ${RANDOM_SEED} -u ${UNAME} -f ${LOGFILE} -m ${MODE} --predict_weight ${PREDICT_WEIGHT}"
-
-    elif [ "${UNAME}" == "zawa-cpu" ]; then
-	PATCH_NAME="20220902_${UNAME}.patch"
-	ADD_COMMAND="curl -LJO https://raw.githubusercontent.com/seigot/tools/master/tetris/${PATCH_NAME} &&\
-	patch -p1 < ${PATCH_NAME}"
-	EXEC_COMMAND="${ADD_COMMAND} && python3 start.py -l ${LEVEL} -d ${DROP_SPEED} -t ${GAME_TIME} -r ${RANDOM_SEED} -u ${UNAME} -f ${LOGFILE} -m ${MODE} --predict_weight ${PREDICT_WEIGHT}"
-	
     elif [ "${UNAME}" == "bushio" ]; then
 	# bushioさん用のtetris
 	ADD_COMMAND="curl -LJO https://raw.githubusercontent.com/seigot/tools/master/tetris/20220901_cuda_unavailable.patch &&\
 	patch -p1 < 20220901_cuda_unavailable.patch"
-	EXEC_COMMAND="${ADD_COMMAND} && python3 start.py -l ${LEVEL} -d ${DROP_SPEED} -t ${GAME_TIME} -r ${RANDOM_SEED} -u ${UNAME} -f ${LOGFILE} -m ${MODE} --predict_weight ${PREDICT_WEIGHT}"
+	EXEC_COMMAND="${ADD_COMMAND} && python3 start.py -l ${LEVEL} -t ${GAME_TIME} -r ${RANDOM_SEED} -u ${UNAME} -f ${LOGFILE} -m ${MODE} --predict_weight ${PREDICT_WEIGHT}"
 
-    elif [ "${UNAME}" == "mattshamrock" -o "${UNAME}" == "usamin24" -o  "${UNAME}" == "isshy-you" -o "${UNAME}" == "yuin0" -o "${UNAME}" == "4321623" -o "${UNAME}" == "kyad" ]; then
-	ADD_COMMAND="ls"
-	EXEC_COMMAND="${ADD_COMMAND} && python3 start.py -l ${LEVEL} -d ${DROP_SPEED} -t ${GAME_TIME} -r ${RANDOM_SEED} -u ${UNAME} -f ${LOGFILE}"
     else
 	# other
-	EXEC_COMMAND="python3 start.py -l ${LEVEL} -d ${DROP_SPEED} -t ${GAME_TIME} -r ${RANDOM_SEED} -u ${UNAME} -f ${LOGFILE} -m ${MODE} --predict_weight ${PREDICT_WEIGHT}"
+	PREDICT_WEIGHT_OPTION="--predict_weight ${PREDICT_WEIGHT}"
+	if [ "${PREDICT_WEIGHT}" == "default" ]; then
+	    PREDICT_WEIGHT_OPTION=""
+	fi
+	#-d ${DROP_SPEED}
+	EXEC_COMMAND="python3 start.py -l ${LEVEL} -t ${GAME_TIME} -r ${RANDOM_SEED} -u ${UNAME} -f ${LOGFILE} -m ${MODE} ${PREDICT_WEIGHT_OPTION}"
     fi
     echo ${EXEC_COMMAND}
 }
@@ -100,7 +49,8 @@ function GET_COMMAND_ART(){
     ART_CONFIG_FILEPATH=${9}
 
     # other
-    EXEC_COMMAND="python3 start.py -l ${LEVEL} -d ${DROP_SPEED} -t ${GAME_TIME} -r ${RANDOM_SEED} -u ${UNAME} -f ${LOGFILE} -m ${MODE} --art_config_filepath ${ART_CONFIG_FILEPATH}"
+    # -d ${DROP_SPEED}
+    EXEC_COMMAND="python3 start.py -l ${LEVEL} -t ${GAME_TIME} -d ${DROP_SPEED} -r ${RANDOM_SEED} -u ${UNAME} -f ${LOGFILE} -m ${MODE} --art_config_filepath ${ART_CONFIG_FILEPATH}"
     echo ${EXEC_COMMAND}
 }
 
